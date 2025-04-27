f15b7a00a1b3950e5751d71d4babcafa804eb20c
Aaron <Aaron9034@Hotmail.com>
2025-04-27 00:31:13 -0700

    add __version__

--- petra/__init__.py
+++ petra/__init__.py
@@ -1,3 +1,5 @@
-from .samples_io import load_samples
-from .make_catalog import make_catalog_mv_normal
-from .posterior_chain import PosteriorChain
+__version__ = "1.0.0"  # don't forget to update this in pyproject.toml
+
+from .samples_io import load_samples  # noqa: F401
+from .make_catalog import make_catalog_mv_normal  # noqa: F401
+from .posterior_chain import PosteriorChain  # noqa: F401
